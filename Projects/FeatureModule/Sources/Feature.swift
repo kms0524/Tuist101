b498a496275d6b6db78110857a3f92d37b205497
kms0524 <gcong2011@gmail.com>
2022-10-13 16:59:13 +0900

    [UPDATE] Delete unnecessary files

--- Projects/FeatureModule/Sources/Feature.swift
+++
@@ -1,8 +0,0 @@
-//
-//  Feature.swift
-//  ProjectDescriptionHelpers
-//
-//  Created by 강민성 on 2022/10/10.
-//
-
-import Foundation
--- Projects/ServiceModule/Sources/Service.swift
+++
@@ -1,8 +0,0 @@
-//
-//  Service.swift
-//  ProjectDescriptionHelpers
-//
-//  Created by 강민성 on 2022/10/10.
-//
-
-import Foundation
